f52bbc21ed06f4e0f7d3c08ece2d2d601fefc742
Celery-gan <1414419932@qq.com>
2019-12-19 09:16:20 +0800

    first

--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import service from "./http"
+import dayjs from "dayjs"
+import axios from 'axios'
+
+Vue.use(ElementUI)
+
+Vue.prototype.$axios = service
+Vue.prototype.$dayjs = dayjs
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
